c6b901aeeaffd45f8a6b3cfbe80431995600c33c
Helen Khumthong <helenkhumthong@gmail.com>
2017-03-07 14:25:28 -0800

    Modify puts methods in hamsters.rb
        - Add conditional operators to string interpolations to correct output.

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -5,7 +5,7 @@ puts "Please rate the #{name}'s noise level on a scale of 1 to 10."
 volume = gets.chomp.to_i
 
 puts "Please enter #{name}'s fur colour."
-colour = gets.chomp.downcase
+colour = gets.chomp.capitalize
 
 puts "Is #{name} suitable for adoption. Enter 'Y' for yes, and 'N' for no."
 adoption = gets.chomp.upcase
@@ -21,9 +21,9 @@ if age == 0.0
     age = nil
 end
 
-puts "Here a summary of the data entered for #{name}."
+puts "Here a summary of the data enteredd for #{name}."
 puts "Name: #{name}"
 puts "Volume: #{volume}"
 puts "Colour: #{colour}"
-puts "Adoption Suitability: #{adoption}"
-puts "Estimated Age: #{age}"
+puts "Adoption Suitability: #{adoption ? 'Yes' : 'No'}"
+puts "Estimated Age (Years): #{age.nil? ? 'No Estimate' : age}"
