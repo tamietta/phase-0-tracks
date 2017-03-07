d43e8aa2b66517ae424eb963efe53f2deab6e464
Helen Khumthong <helenkhumthong@gmail.com>
2017-03-07 12:42:28 -0800

    Modify program in hamster.rb

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -1,20 +1,31 @@
-puts "What is your hampsters name?"
+puts "What is your hamsters name?"
 name = gets.chomp
-puts "What is your hampsters volume level (1-10)?"
-volume = gets.chomp
-puts "What is your hampsters fur color?"
+puts "What is your hamsters volume level (1-10)?"
+volume = gets.chomp.to_f
+puts "What is your hamsters fur color?"
 fur_color = gets.chomp
-puts "is your hamster a good condidate for adoption?"
+puts "is your hamster a good condidate for adoption? If yes, enter 'Y'; if no, enter 'N'."
 adoption_condition = gets.chomp
-puts "What is your hampsters estimated age? (feel free to leave blank)"
+if adoption_condition == 'Y'
+    adoption_condition = true
+else
+    adoption_condition = false
+end
+    
+puts "What is your hamsters estimated age? (feel free to leave blank)"
 age = gets.chomp
 if age == ""
 	age = nil
 else
-	age = age
+	age = age.to_f
 end
-p name
-p volume
-p fur_color
-p adoption_condition
-p age
+
+puts "Hamster's Name: #{name}"
+puts "Volume: #{volume}" 
+puts "Fur Color: #{fur_color}"
+puts "Adoption Condition: #{adoption_condition}" 
+if age == nil
+    puts "Age: no response"
+else
+    puts "Age: #{age}"
+end
