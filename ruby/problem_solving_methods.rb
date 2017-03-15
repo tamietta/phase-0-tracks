25690cac538bb09b0ae8fba56ffb2d7d762172d9
Helen Khumthong <helenkhumthong@gmail.com>
2017-03-15 14:42:19 -0700

    Add fibonacci method to problem_solving_methods.rb

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -1,7 +1,7 @@
 arr  = [42, 89, 23, 1]
 
 def search(array, integer)
-    return_index = nil
+    return_index = nils
     array.length.times do |idx|
       if array[idx] == integer
         return_index = idx
@@ -11,3 +11,24 @@ def search(array, integer)
 end
 
 p search(arr, 10)
+
+def fib(n)
+  fib_array = [0, 1]
+  if n == 1
+    return fib_array[0..0]
+  elsif n == 2
+    return fib_array[0..1]
+  else
+    (n-2).times do 
+      new_fib = fib_array[-2] + fib_array[-1]
+      fib_array.push(new_fib)
+    end
+  end
+  fib_array
+end
+
+p fib(1)
+p fib(2)
+p fib(3)
+p fib(6)
+p (fib(100)[-1] == 218922995834555169026)
