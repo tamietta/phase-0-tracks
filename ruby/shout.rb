84263ffc85ee99f40b124f7a4cff3a058570c813
Helen Khumthong <helenkhumthong@gmail.com>
2017-03-18 12:11:25 -0700

    Modify Shout module, and add two new classes to shout.rb

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,3 +1,5 @@
+=begin
+
 module Shout
   def self.yell_angrily(words)
     words + "!!! " + ":("
@@ -13,3 +15,34 @@ end
 
 p Shout.yell_angrily("This is so annoying")
 p Shout.yelling_happily("I did")
+
+=end
+
+module Shout
+  def yell_angrily(words)
+    words + "!!! " + ":("
+  end
+
+  def yelling_happily(words)
+    words + "!!! " + ":)"
+  end
+end
+
+class Programmer
+  include Shout
+end
+
+class Student
+  include Shout
+end
+
+# TEST CODE
+
+programmer = Programmer.new
+puts programmer.yell_angrily("Why isn't this working")
+puts programmer.yelling_happily("I solved it")
+
+student = Student.new
+puts student.yell_angrily("This is so complicated")
+puts student.yelling_happily("This finally makes sense")
+
