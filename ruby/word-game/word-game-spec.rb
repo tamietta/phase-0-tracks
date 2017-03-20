e53f30b9945355a22d0a564b78499b495fcdf2d1
Helen Khumthong <helenkhumthong@gmail.com>
2017-03-20 14:46:25 -0700

    Prepare code for program continuation in word-game.rb and word-game-spec.rb

--- ruby/word-game/word-game-spec.rb
+++ ruby/word-game/word-game-spec.rb
@@ -1,10 +1,12 @@
 # RSpec
-# describe <OBJECT> do
-    # let(:<VARIABLE>) {<CODE>}
-    # it "<DESCRIPTION>" do
-        # expect(<YOUR CODE>).to eq <RESULT>
-    # end
-# end
+# require_relative(<FILE>)
+# describe(description, &block)
+  # let(name, &block)
+  # subject(name, &block)
+    # context(description, &block)
+      # it(description, &block)
+        # skip(reason)
+        # expect(<ACTUAL>).to eq <EXPECTED>
 
 require_relative 'word-game'
 
@@ -28,36 +30,36 @@ describe WordGame do
     end
 
     it "sets guessed letter to empty array" do
-      expect(wordgame.guesses).to eq []
+      expect(wordgame.guesses).to eq [1]
     end
 
   end
 
-   context "when guess given" do
+   # context "when guess given" do
 
-    let(:wordgame) { WordGame.new("secret") }
+   #  let(:wordgame) { WordGame.new("secret") }
 
-    it "checks if guess correct" do
-      expect(wordgame.check_guess "c").to eq true
-      expect(wordgame.check_guess "f").to eq false
-    end
+   #  it "checks if guess correct" do
+   #    expect(wordgame.check_guess "c").to eq true
+   #    expect(wordgame.check_guess "f").to eq false
+   #  end
 
-    it "adds guess to past guesses" do
-      expect(wordgame.add_guess "c").to eq ["c"]
-      expect(wordgame.add_guess "d").to eq ["c", "d"]
-    end
+   #  it "adds guess to past guesses" do
+   #    expect(wordgame.add_guess "c").to eq ["c"]
+   #    expect(wordgame.add_guess "d").to eq ["c", "d"]
+   #  end
 
-    it "checks if guess duplicated" do
-      wordgame.add_guess "c"
-      expect(wordgame.duplicate? "e").to eq false
-      expect(wordgame.duplicate? "c").to eq true 
-    end
+   #  it "checks if guess duplicated" do
+   #    wordgame.add_guess "c"
+   #    expect(wordgame.duplicate? "e").to eq false
+   #    expect(wordgame.duplicate? "c").to eq true 
+   #  end
 
     # it "updates word state" do
     #   expect(wordgame_test.update_word_state "c").to eq "_ _ c _ _ _"
     #   expect(word_test.update_word_state "d").to eq
     # end
 
-  end
+  # end
 
 end
