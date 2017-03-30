b5cceb79990ce3f32e8fbee98103d4bc80aa7511
Helen Khumthong <helenkhumthong@gmail.com>
2017-03-30 16:02:41 -0700

    Add answers as comments at the end of data_structures.js.

--- js/data_structures.js
+++ js/data_structures.js
@@ -30,3 +30,36 @@ console.log(honda);
 console.log(ford);
 
 honda.accelerate(160)
+
+// Assigment Questions
+
+// 1. If you wanted to loop through the keys and values of an object, how would you do that?
+
+obj = {a: 1, b: 2, c: 3}
+
+// Key-Value Iteration Method #1
+
+for (var k in obj) {
+  if (obj.hasOwnProperty(k)) {
+    console.log(k, ": ", obj[k]);
+  }
+}
+
+// Key-Value Iteration Method #2
+
+for (var [k, v] in Object.entries(obj)) {
+  console.log(k, ":", v);
+}
+
+// 2. Are there advantages to using constructor functions to create objects? Disadvantages? 
+
+// Advantage:
+//  - Facilitates replication of objects with same set of properties
+//  - Enables inheritance of properties in prototype chain (if desired).
+//  - Enables declaration of private properties and methods.
+
+// Disadvantage:
+//  - If replication not needed, constructor method is superfluous code.
+//  - Property names between instance-owned and prototype properties can inadvertently collide.
+//  - Use of for..in loops will iterate over prototype properties, unless conditional is set (i.e. < if (object.hasOwnProperty(key)) {} >).
+
