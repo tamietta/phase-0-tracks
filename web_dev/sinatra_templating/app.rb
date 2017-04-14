031f49b375bd3455118a25dfa0fd6b65840b75a6
Helen Khumthong <helenkhumthong@gmail.com>
2017-04-14 16:43:03 -0700

    Add complete and working code to app.rb, campus.erb and campus_form.erb.
        - app.rb: add method to retrieve and display results (l.36-45)
    
        - campus.erb: display results in table (l.9-23)
    
        - campus_form.erb: change placeholder (l.10)

--- web_dev/sinatra_templating/app.rb
+++ web_dev/sinatra_templating/app.rb
@@ -2,36 +2,45 @@
 require 'sinatra'
 require 'sqlite3'
 
-# add static resources
-# set default static folder for CSS and JS files.
+# Add static resources (CSS and JS files), by setting default folder.
 set :public_folder, File.dirname(__FILE__) + '/static'
 
 db = SQLite3::Database.new("students.db")
 db.results_as_hash = true
 
-# show students on the home page
+# Display home page
 get '/' do
   @students = db.execute("SELECT * FROM students")
   erb :home
 end
 
+# Display new student form page
 get '/students/new' do
   erb :new_student
 end
 
-# create new students via
-# a form
+# Create new students via form
 post '/students' do
-  db.execute("INSERT INTO students (name, campus, age) VALUES (?,?,?)", [params['name'], params['campus'], params['age'].to_i])
+  db.execute(
+    "INSERT INTO students (name, campus, age) VALUES (?,?,?)", 
+    [params['name'], params['campus'], params['age'].to_i])
   redirect '/'
 end
 
+# Display campus form page
 get '/students/campus_form' do
   erb :campus_form
 end
 
+# Display campus results page
 post '/students/campus' do
-  @campus = params[:campus]
+  # Set SQL result-set as array only within the scope of method.
+  db.results_as_hash = false
+  @cols, *@rows = db.execute2(
+    "SELECT * FROM students WHERE campus = ?", params[:campus])
   erb :campus
 end
+
+# Verify result-set as hash in global scope
+p db.execute("SELECT * FROM students WHERE id = 1")
 
